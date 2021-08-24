0b31607a2466602824059a50a2ccc708994ced2e
Douglas Macedo <dodop3000@gmail.com>
2021-08-24 11:58:12 -0300

    Seed db with 1 million users with 0 points.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Points.Repo
+alias Points.Accounts
+
+data =
+  Enum.map(1..1_000_000, fn _ -> %{points: 0} end)
+  |> Enum.map(fn row ->
+    now = NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
+
+    row
+    |> Map.put(:inserted_at, now)
+    |> Map.put(:updated_at, now)
+  end)
+
+list_of_chunks = Enum.chunk_every(data, 10_000)
+
+Enum.each(list_of_chunks, fn rows ->
+  Repo.insert_all(Accounts.User, rows)
+end)
