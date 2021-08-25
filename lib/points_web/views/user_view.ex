6696afd41740e43da4c735ee864cceb24907d878
Douglas Macedo <dodop3000@gmail.com>
2021-08-25 12:19:03 -0300

    Stringify timestamp for a more human-friendly format

--- lib/points_web/views/user_view.ex
+++ lib/points_web/views/user_view.ex
@@ -3,10 +3,16 @@ defmodule PointsWeb.UserView do
   alias PointsWeb.UserView
 
   def render("index.json", %{users: users, timestamp: timestamp}) do
-    %{users: render_many(users, UserView, "user.json"), timestamp: timestamp}
+    %{
+      users: render_many(users, UserView, "user.json"),
+      timestamp: stringify_timestamp(timestamp)
+    }
   end
 
   def render("user.json", %{user: user}) do
     %{id: user.id, points: user.points}
   end
+
+  defp stringify_timestamp(timestamp) when is_nil(timestamp), do: nil
+  defp stringify_timestamp(timestamp), do: NaiveDateTime.to_string(timestamp)
 end
